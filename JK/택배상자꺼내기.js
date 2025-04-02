58125b380f9a008d08e0c778b633759687d41436
Littlestar0508 <rist9860@gmail.com>
2025-04-02 21:16:27 +0900

    택배상자꺼내기

---
+++ JK/택배상자꺼내기.js
@@ -0,0 +1,16 @@
+function solution(n, w, num) {
+  let answer = 0;
+
+  const max_h = Math.ceil(n / w);
+  const cur_h = Math.ceil(num / w);
+
+  let max_line = n % w || n;
+  let cur_line = num % w || num;
+
+  answer = max_h - cur_h;
+
+  if (max_h % 2 === cur_h % 2 && max_line >= cur_line) answer++;
+  if (max_h % 2 !== cur_h % 2 && max_line + cur_line > w) answer++;
+
+  return answer;
+}

0c37a83578cdc172a7354acd76648d4492c34953
Littlestar0508 <rist9860@gmail.com>
2025-04-17 10:50:00 +0900

    2차원 동전 뒤집기

--- JK/2차원동전뒤집기.js
+++ JK/2차원동전뒤집기.js
@@ -1,69 +1,52 @@
-// 미해결
+// https://school.programmers.co.kr/questions/44033 참고자료
+
+// 기존 방식 -> 모든 행과 열을 한 번만 뒤집는다고 가정하고 계산했지만, 시간 초과 + 미해결 케이스들 발생
+
+// 해결하기 위한 방식
+// 각 행의 첫 번째 원소가 서로 다르면 행을 먼저 뒤집는다. 이후 각 열의 첫 번째 원소가 다르면 열을 뒤집는다.
+// 각 행의 첫 번째 원소가 서로 같으면 행을 먼저 뒤집는다. 이후 각 열의 첫 번째 원소가 다르면 열을 뒤집는다.
+// 각 열의 첫 번째 원소가 서로 다르면 열을 먼저 뒤집는다. 이후 각 행의 첫 번째 원소가 다르면 행을 뒤집는다.
+// 각 열의 첫 번째 원소가 서로 같으면 열을 먼저 뒤집는다. 이후 각 행의 첫 번째 원소가 다르면 행을 뒤집는다.
+// 를 적용
 
 function solution(beginning, target) {
-  const chk = Array.from(Array(beginning.length), () => new Array(beginning[0].length).fill(false));
-  const visited = Array.from(Array(2), () => new Array(beginning[0].length).fill(false));
+  const N = beginning.length;
+  const M = beginning[0].length;
+  let answer = Infinity;
 
-  while (!isAllVisited(visited)) {
-    const row_col = Array.from(Array(2), () => new Array(beginning[0].length).fill(0));
+  // 행(불일치)열(불일치) , 행(불일치)열(일치) , 행(일치)열(불일치) , 행(일치)열(일치) 검사 진행
+  for (let c = 0; c < 4; c++) {
+    let copy = JSON.parse(JSON.stringify(beginning));
+    let cnt = 0;
 
-    for (let i = 0; i < beginning.length; i++) {
-      for (let j = 0; j < beginning[0].length; j++) {
-        chk[i][j] = beginning[i][j] === target[i][j];
-        if (!chk[i][j]) {
-          row_col[0][i]++;
-          row_col[1][j]++;
+    // 행 뒤집기
+    for (let i = 0; i < N; i++) {
+      if ((c < 2 && copy[i][0] !== target[i][0]) || (c >= 2 && copy[i][0] === target[i][0])) {
+        cnt++;
+        for (let j = 0; j < M; j++) {
+          copy[i][j] = 1 - copy[i][j];
         }
       }
     }
 
-    console.log(row_col);
+    // 열 뒤집기
+    for (let i = 0; i < M; i++) {
+      if ((c % 2 === 0 && copy[0][i] !== target[0][i]) || (c % 2 === 1 && copy[0][i] === target[0][i])) {
+        cnt++;
+        for (let j = 0; j < N; j++) {
+          copy[j][i] = 1 - copy[j][i];
+        }
+      }
+    }
 
-    const max_idx = find_max_idx(row_col, visited);
-
-    if (max_idx > 4) {
-      change_col(beginning, max_idx - 5);
-      visited[1][max_idx - 5] = true;
-    } else {
-      change_row(beginning, max_idx);
-      visited[0][max_idx] = true;
+    // 배열 검사
+    if (JSON.stringify(copy) === JSON.stringify(target)) {
+      answer = Math.min(cnt, answer);
     }
   }
 
-  console.log(1);
-}
-
-function change_row(arr, n) {
-  for (let i = 0; i < arr.length; i++) {
-    arr[n][i] = !arr[n][i];
-  }
-}
-
-function change_col(arr, n) {
-  for (let i = 0; i < arr.length; i++) {
-    arr[i][n] = !arr[i][n];
-  }
-}
-
-function find_max_idx(arr, visited) {
-  const list = arr.flat();
-  const max = find_max(list);
-
-  return list.indexOf(max);
-}
-
-function find_max(arr) {
-  return Math.max(...arr);
-}
-
-function isAllVisited(arr) {
-  for (let i = 0; i < arr.length; i++) {
-    for (let j = 0; j < arr[0].length; j++) {
-      if (!arr[i][j]) return false;
-    }
-  }
-
-  return true;
+  // 모든 검사를 하고도 일치하는 케이스가 없었다면 -1 반환
+  return answer === Infinity ? -1 : answer;
 }
 
 console.log(

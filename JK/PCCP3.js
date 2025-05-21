13f9f2f0c29bfe056e1a425c38625185157c1ac3
Littlestar0508 <rist9860@gmail.com>
2025-05-21 11:14:56 +0900

    PCCP 3번 기출문제 주석

--- JK/PCCP3.js
+++ JK/PCCP3.js
@@ -1,9 +1,13 @@
+// 한 자리만 움직이는 경우의 함수
 function rotateSingle(arr, num, dir, pos) {
   const digit = num.split("").map(Number);
 
   const result = [...digit];
   let carry = dir;
 
+  // carry는 자신보다 높은 단계에 있는 곳에만 영향을 끼치므로
+  // 전달받은 position으로 부터 상위 단계만 탐색하고
+  // carry가 일어나지 않는다면 반복문 종료
   for (let i = pos; i >= 0; i--) {
     if (carry === 0) break;
     result[i] += carry;
@@ -22,9 +26,11 @@ function rotateSingle(arr, num, dir, pos) {
   return result.join("");
 }
 
+// 전체를 돌리는 경우
 function rotateMulti(arr, num, dir) {
   const digit = num.split("").map(Number);
 
+  // 각각의 자리수를 방문해서 조건문에 따라 숫자를 다르게(carry가 일어나지 않으므로 간단한 로직)
   const result = digit.map((item, idx) => {
     let next = item + dir;
     if (next > arr[idx]) return 0;
@@ -36,8 +42,10 @@ function rotateMulti(arr, num, dir) {
 }
 
 function solution(arr, num, password) {
+  // -1과 1의 방향으로 bfs 탐색
   const dx = [-1, 1];
 
+  // 방문 Set
   const visited = new Set();
   const queue = [[num, 0]];
   visited.add(num);
@@ -49,6 +57,7 @@ function solution(arr, num, password) {
 
     for (let i = 0; i < arr.length; i++) {
       for (let dir = 0; dir < 2; dir++) {
+        // 하나의 자리수만 돌리는 경우와 모두 돌리는 경우 둘 다 체크
         const next = rotateSingle(arr, cur, dx[dir], i);
 
         if (!visited.has(next)) {
@@ -68,7 +77,10 @@ function solution(arr, num, password) {
     }
   }
 
+  // 도달하지 못할 것을 대비하여 -1 출력
   return -1;
 }
 
-console.log(solution([3, 4, 5, 6], "1111", "3456"));
+// console.log(solution([3, 4, 5, 6], "1111", "3456"));
+
+console.log(rotateSingle([3, 4, 5, 6], "1111", -3, 3));

e474a1039b186b19346fa9ff834220dc7d8f1b89
Littlestar0508 <rist9860@gmail.com>
2025-05-22 10:57:08 +0900

    PCCP 4번 문제

--- JK/PCCP4.js
+++ JK/PCCP4.js
@@ -2,13 +2,17 @@ function solution(k, node, edge) {
   let answer = 0;
 
   function dfs(left, right, cur, depth) {
-    if (depth === k) return;
+    if (depth === k) {
+      answer++;
+      return;
+    }
 
     for (let i = 0; i < node.length; i++) {
       if (i + 1 === cur) continue;
       if (node[i] === "(") {
-        if (k / 2 >= left) continue;
+        if (k / 2 <= left) continue;
         const leftArr = edge.filter((item) => item.includes(cur) && item.includes(i + 1));
+        console.log(leftArr);
         for (let j = 0; j < leftArr.length; j++) {
           const [x, y] = leftArr[j];
           if (x === cur) dfs(left + 1, right, y, depth + 1);
@@ -18,10 +22,10 @@ function solution(k, node, edge) {
       if (node[i] === ")") {
         if (left <= right) continue;
         const rightArr = edge.filter((item) => item.includes(cur) && item.includes(i + 1));
-        for (let j = 0; j < rightArr; j++) {
+        for (let j = 0; j < rightArr.length; j++) {
           const [x, y] = rightArr[j];
-          if (x === cur) dfs(left, right + 1, y, depth);
-          if (y === cur) dfs(left, right + 1, x, depth);
+          if (x === cur) dfs(left, right + 1, y, depth + 1);
+          if (y === cur) dfs(left, right + 1, x, depth + 1);
         }
       }
     }
@@ -30,6 +34,8 @@ function solution(k, node, edge) {
   for (let i = 0; i < node.length; i++) {
     if (node[i] === "(") dfs(1, 0, i + 1, 1);
   }
+
+  return answer;
 }
 
 console.log(

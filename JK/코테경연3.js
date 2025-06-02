b05f7bffe4a57e237b234915f0c51a694a9ff5a9
Littlestar0508 <rist9860@gmail.com>
2025-06-02 12:17:19 +0900

    indexOf 시간 복잡도 문제로 queue 형태로 대체

--- JK/코테경연3.js
+++ JK/코테경연3.js
@@ -2,37 +2,31 @@ function solution(N, M, S, K, culture, edge) {
   const length = Array(N).fill(-1);
   const visited = Array(N).fill(-1);
 
+  const indexArr = [];
+
   for (let i = 0; i < culture.length; i++) {
     length[culture[i] - 1] = 0;
     visited[culture[i] - 1] = 0;
+    indexArr.push(culture[i]);
   }
 
-  while (true) {
-    if (visited.indexOf(-1) === -1) break;
+  while (indexArr.length > 0) {
+    const index = indexArr.shift();
+    visited[index - 1] = 1;
 
-    const indexArr = [];
-    while (true) {
-      if (visited.indexOf(0) === -1) break;
-      idx = visited.indexOf(0) + 1;
-      indexArr.push(idx);
-      visited[idx - 1] = 1;
-    }
+    const connected = edge.filter((item) => item.includes(index));
 
-    for (let i = 0; i < indexArr.length; i++) {
-      const index = indexArr[i];
-
-      const connected = edge.filter((item) => item.includes(index));
-
-      for (let j = 0; j < connected.length; j++) {
-        const [x, y] = connected[j];
-        if (x === index && visited[y - 1] === -1) {
-          length[y - 1] = length[x - 1] + 1;
-          visited[y - 1] = 0;
-        }
-        if (y === index && visited[x - 1] === -1) {
-          length[x - 1] = length[y - 1] + 1;
-          visited[x - 1] = 0;
-        }
+    for (let j = 0; j < connected.length; j++) {
+      const [x, y] = connected[j];
+      if (x === index && visited[y - 1] === -1) {
+        length[y - 1] = length[x - 1] + 1;
+        visited[y - 1] = 0;
+        indexArr.push(y);
+      }
+      if (y === index && visited[x - 1] === -1) {
+        length[x - 1] = length[y - 1] + 1;
+        visited[x - 1] = 0;
+        indexArr.push(x);
       }
     }
   }

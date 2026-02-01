305b3807b96bdebe74606ffb749dc3852796a5ef
Littlestar0508 <rist9860@gmail.com>
2026-02-01 10:49:53 +0900

    BOJ18258

---
+++ JK/BOJ18258.js
@@ -0,0 +1,58 @@
+const fs = require("fs");
+const path = require("path");
+
+let input = fs
+  .readFileSync(
+    process.platform === "linux"
+      ? "/dev/stdin"
+      : path.join(__dirname, "test.txt"),
+  )
+  .toString()
+  .trim()
+  .split(/\r?\n/);
+
+const map = new Map();
+const answer = [];
+let idx = 1;
+let frontIdx = 1;
+
+for (let i = 1; i <= +input[0]; i++) {
+  const [order, num] = input[i].split(" ");
+
+  switch (order) {
+    case "push":
+      map.set(idx, +num);
+      idx++;
+      break;
+
+    case "pop":
+      if (map.size === 0) answer.push(-1);
+      else {
+        answer.push(map.get(frontIdx));
+        map.delete(frontIdx);
+        frontIdx++;
+      }
+      break;
+
+    case "size":
+      answer.push(map.size);
+      break;
+
+    case "empty":
+      if (map.size === 0) answer.push(1);
+      else answer.push(0);
+      break;
+
+    case "front":
+      if (map.size === 0) answer.push(-1);
+      else answer.push(map.get(frontIdx));
+      break;
+
+    case "back":
+      if (map.size === 0) answer.push(-1);
+      else answer.push(map.get(idx - 1));
+      break;
+  }
+}
+
+console.log(answer.join("\n"));
